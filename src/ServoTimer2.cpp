9d69584e3bc99916f715aac167b3e41f8efa9473
flowey-nuc <stepanmasirov@gmail.com>
2024-02-17 13:45:49 +0500

    fix again

--- src/ServoTimer2.cpp
+++ src/ServoTimer2.cpp
@@ -3,7 +3,7 @@
 #include <Arduino.h>
 
 #define FRAME_SYNC_INDEX  0        //frame sync delay is the first entry in the channel array
-#define FRAME_SYNC_PERIOD 12750    //total frame duration in microseconds
+#define FRAME_SYNC_PERIOD 16750    //total frame duration in microseconds
 #define FRAME_SYNC_DELAY  ((FRAME_SYNC_PERIOD - (NBR_CHANNELS * DEFAULT_PULSE_WIDTH)) / 128) //number of iterations of the ISR to get the desired frame rate
 #define DELAY_ADJUST      8        //number of microseconds of calculation overhead to be subtracted from pulse timings
 
